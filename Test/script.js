9cffaaa311f1c1fec9834d2ed83a0deb1a52d391
Kale Baker <kbaker624@canon-slade.bolton.sch.uk>
2024-11-11 10:51:16 +0000

    rectangular Area Calculator

---
+++ Test/script.js
@@ -0,0 +1,7 @@
+function inputfunction() {
+    var side1 = document.getElementById("Input1").value;
+    var side2 = document.getElementById("Input2").value;
+    var result = side1 * side2
+    answernum.innerHTML = result
+}
+
